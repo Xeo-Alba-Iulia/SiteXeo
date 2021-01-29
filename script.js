2fa61cec9ff99ce621ef3ea77722908a5212b101
Dexter0-0 <56682055+Dexter0-0@users.noreply.github.com>
2021-01-29 16:14:17 +0200

    Update

--- script.js
+++ script.js
@@ -3,44 +3,14 @@
 /* Window loaded main function */
 window.addEventListener('load', function() 
 {
-    UpdateNavbarMenu();
+
 });
 
 
 /* Window resizes main function */
 window.addEventListener('resize', function() 
 {
-    UpdateNavbarMenu();
     
 });
 
-// Switches between the phone and pc menus based on the width of the page
-CurrentMenu = "PC";
-PCMenu = document.getElementById("NavbarMenu").outerHTML;
-PhoneMenu = document.getElementById("NavbarMenuPhone").outerHTML;
-document.getElementById("NavbarMenu").outerHTML = "";
-document.getElementById("NavbarMenuPhone").outerHTML = "";
-NavbarContainer = document.getElementById("NavbarContainer");
-NavbarContainerInner = NavbarContainer.innerHTML;
-
-function UpdateNavbarMenu()
-{
-    if(window.matchMedia("(max-width: 1000px)").matches)
-    {
-        CurrentMenu = "Phone";
-        NavbarContainer.innerHTML = NavbarContainerInner + PhoneMenu;
-    }
-    else
-    {
-        CurrentMenu = "PC";
-        NavbarContainer.innerHTML = NavbarContainerInner + PCMenu;
-    }
-}
-
-/* Window scrolling main function */
-window.addEventListener("scroll", function()
-{
-    
-
-});
 
