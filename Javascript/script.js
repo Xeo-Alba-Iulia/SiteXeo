19c0218a03f610c64dfc0e07f7e07083be9055c8
Dexter0-0 <56682055+Dexter0-0@users.noreply.github.com>
2021-02-16 09:08:59 +0200

    a

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,6 +1,6 @@
 /*************************************** | Window loaded event | ****************************************/
 
-window.addEventListener('load', function() 
+window.addEventListener("load", function() 
 {
     ResizeSponsorsRobot(document.body.clientWidth);
 
@@ -8,7 +8,7 @@ window.addEventListener('load', function()
 
 /*************************************** | Window resizes event | ****************************************/
 
-window.addEventListener('resize', function() 
+window.addEventListener("resize", function() 
 {
     SiteWidth = document.body.clientWidth;
 
@@ -33,9 +33,70 @@ function ResizeSponsorsRobot(SiteWidth)
     SponsorsRobotContainer.style.width = SiteWidth + "px";
 }
 
+/*************************************** | Window scroll event | ****************************************/
+
+var CurrentActiveMenu = "HomeLink";
+
+window.addEventListener("scroll", function() 
+{
+    UpdateNavigationBarActive();
+
+
+});
+
+// Updates the active item in the Navigation bar depending on wich section of the site is on the screen
+function UpdateNavigationBarActive()
+{
+    var HomeElement = document.getElementById("HomeElement");
+    var AboutUsElement = document.getElementById("AboutUsElement");
+    var SponsorsElement = document.getElementById("SponsorsElement");
+    var BlogElement = document.getElementById("BlogElement");
+    var ContactElement = document.getElementById("ContactElement");
+    var ActiveElement = document.getElementById(CurrentActiveMenu);
+
+    if(CurrentActiveMenu != "HomeLink" && IsVisible(HomeElement))
+    {
+        UpdateCurrentActive(ActiveElement, "HomeLink");
+    }
+    else if(CurrentActiveMenu != "AboutUsLink" && IsVisible(AboutUsElement))
+    {
+        UpdateCurrentActive(ActiveElement, "AboutUsLink");
+    }
+    else if(CurrentActiveMenu != "SponsorsLink" && IsVisible(SponsorsElement))
+    {
+        UpdateCurrentActive(ActiveElement, "SponsorsLink");
+    }
+}
+
+// Helper function for UpdateNavigationBarActive(). Returns true if an element is in the Virewport or false otherwise
+function IsVisible(Element) 
+{
+	var Distance = Element.getBoundingClientRect();
+	
+	if(Distance.top >= 0 && Distance.left >= 0 && Distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) && Distance.right <= (window.innerWidth || document.documentElement.clientWidth))
+	{
+		return true;
+	}
+	else 
+	{
+		return false;
+	}
+};
+
+// Helper function for UpdateNavigationBarActive(). It updates the currently active element.
+function UpdateCurrentActive(ActiveElement, NewActiveElement)
+{
+    ActiveElement.classList.remove("ActiveLink");
+    ActiveElement.classList.add("AnimationUnderline");
+    document.getElementById(NewActiveElement).classList.add("ActiveLink"); 
+    CurrentActiveMenu = NewActiveElement;
+}
+
 /*************************************** | JQuerry | ****************************************/
 
-$(function() 
+$(function()
 {
     SponsorsRobotAnimation();
 })
+
+
