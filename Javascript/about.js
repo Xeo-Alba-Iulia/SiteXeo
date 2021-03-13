cd734aa5c0e8dd17f6ba6550cf59201c7bb6ec26
Dexter0-0 <56682055+Dexter0-0@users.noreply.github.com>
2021-03-13 09:50:27 +0200

    dsdsad

--- Javascript/about.js
+++ Javascript/about.js
@@ -5,5 +5,26 @@
                  - Page.js (code that is specific for that page and that page only wich is not included in the other pages)
 */
 
+var CurrentActiveMenu = "AboutLink";
 
+function UpdateNavigationBarActive()
+{
+    var AboutElement = document.getElementById("AboutElement");
+    var AchievementsElement = document.getElementById("AchievementsElement");
+    var TeamElement = document.getElementById("TeamElement");
+    var ActiveElement = document.getElementById(CurrentActiveMenu);
+
+    if(CurrentActiveMenu != "AboutLink" && IsVisible(AboutElement))
+    {
+        UpdateCurrentActive(ActiveElement, "AboutLink");
+    }
+    else if(CurrentActiveMenu != "AchievementsLink" && IsVisible(AchievementsElement))
+    {
+        UpdateCurrentActive(ActiveElement, "AchievementsLink");
+    }
+    else if(CurrentActiveMenu != "TeamLink" && IsVisible(TeamElement))
+    {
+        UpdateCurrentActive(ActiveElement, "TeamLink");
+    } 
+}
 
